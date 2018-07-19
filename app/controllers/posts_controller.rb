4d0d6dac82c67f70cdcacbe28d77eb2739280329
Jan Hovancik <conta.srdr@gmail.com>
2018-07-19 17:08:28 +0200

    Allows user to add new Post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,24 @@
 class PostsController < ApplicationController
   def new
+    @post = Post.new
     render layout: false
+  end
+
+  def create
+    post = Post.create(post_params)
+    if post.valid?
+      render json: {
+        }, status: 200
+    else
+      render json: {
+        errors: "There seems to be some errors: #{post.errors.full_messages.join(', ')}"
+      }, status: 422
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :url, :author_id, :description)
   end
 end
---
+++ app/models/post.rb
@@ -0,0 +1,9 @@
+class Post < ApplicationRecord
+  belongs_to :author, class_name: 'User'
+  belongs_to :approved_by, class_name: 'User', optional: true
+  belongs_to :declined_by, class_name: 'User', optional: true
+
+  validates :title, presence: true
+  validates :url, presence: true
+  validates :description, presence: true
+end

97d952cd2538d91fd37e44244f2cf0eff9d5eb73
Jan Hovancik <conta.srdr@gmail.com>
2018-07-20 11:56:33 +0200

    Setup tools and tests

--- test/controllers/posts_controller_test.rb
+++ test/controllers/posts_controller_test.rb
@@ -1,9 +1,9 @@
 require 'test_helper'
 
 class PostsControllerTest < ActionDispatch::IntegrationTest
-  test "should get new" do
-    get posts_new_url
-    assert_response :success
-  end
+  # test "should get new" do
+  #   get posts_new_url
+  #   assert_response :success
+  # end
 
 end

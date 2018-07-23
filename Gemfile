9c031d5db451a91121b923739525180c386255bd
Jan Hovancik <conta.srdr@gmail.com>
2018-07-23 14:18:44 +0200

    Allows posts description to have markdown in it
    plus small style refactor

--- Gemfile
+++ Gemfile
@@ -63,3 +63,6 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 # Auth via twitter
 gem 'omniauth-twitter'
+
+# Markdown for posts
+gem 'redcarpet', '~> 3.3', '>= 3.3.4'
--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,7 @@
 module PostsHelper
+  def markdown_to_html(text)
+    renderer = Redcarpet::Render::HTML.new
+    markdown = Redcarpet::Markdown.new(renderer)
+    markdown.render(text)
+  end
 end

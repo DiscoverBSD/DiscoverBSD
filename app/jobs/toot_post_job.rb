0e05452800ba0270f9855df616fe2d2f08b761db
Jan Hovancik <conta.srdr@gmail.com>
2019-01-02 17:18:22 +0100

    Adds tooting to Mastodon

---
+++ app/jobs/toot_post_job.rb
@@ -0,0 +1,14 @@
+# Toot post title and url to Mastodon
+class TootPostJob< ApplicationJob
+  def perform(post)
+    return unless ENV['TOOT_POSTS'].present?
+    client.create_status("#{post.title} \n\n https://discoverbsd.com/p/#{post.slug}")
+  end
+
+  private
+
+  def client
+    @client ||= Mastodon::REST::Client.new(base_url: 'https://bsd.network',
+                  bearer_token: ENV['MASTODON_ACCESS_TOKEN'])
+  end
+end

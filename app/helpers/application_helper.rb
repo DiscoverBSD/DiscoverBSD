3be5f37b2ac604b1692b6d90fb4edcfaee65231b
Jan Hovancik <conta.srdr@gmail.com>
2020-01-03 14:31:00 +0100

    Adds missing image

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,7 +5,7 @@ module ApplicationHelper
           else
             root_url
           end
-    "https://motif.imgix.com/i?url=#{u url}&image_url=#{u image_url('/img/nik-shuliahin-rkFIIE9PxH0-unsplash.jpg') }&color=88898b&logo_url=https%3A%2F%2Fi.ibb.co%2FZTr5683%2Fdiscoverbsd.png&logo_alignment=top%2Ccenter&text_alignment=bottom%2Ccenter&logo_padding=70&font_family=Avenir%20Next%20Demi%2CBold&text_color=1d1d1d"
+    "https://motif.imgix.com/i?url=#{u url}&image_url=#{u image_url('/img/nik-shuliahin-rkFIIE9PxH0-unsplash.jpg') }&color=88898b&logo_url=#{u image_url('/img/discoverbsd.png') }&logo_alignment=top%2Ccenter&text_alignment=bottom%2Ccenter&logo_padding=70&font_family=Avenir%20Next%20Demi%2CBold&text_color=1d1d1d"
   end
 
   def add_utm_source url

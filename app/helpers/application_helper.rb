7b0f70a79ca8c66342cde1fea7b858717574fd3d
Jan Hovancik <conta.srdr@gmail.com>
2021-03-25 13:24:09 +0100

    Improve

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,11 +8,19 @@ module ApplicationHelper
     "https://motif.imgix.com/i?url=#{u url}&image_url=#{u image_url('/img/nik-shuliahin-rkFIIE9PxH0-unsplash.jpg') }&color=88898b&logo_url=#{u image_url('/img/discoverbsd.png') }&logo_alignment=top%2Ccenter&text_alignment=bottom%2Ccenter&logo_padding=70&font_family=Avenir%20Next%20Demi%2CBold&text_color=1d1d1d"
   end
 
-  def add_utm_source url
+  def discoverbsd_utm_source_url url
     if URI(url).query.present?
       url + "&utm_source=discoverbsd"
     else
       url + "?utm_source=discoverbsd"
+    end
+  end
+
+  def bsdweekly_utm_source_url url
+    if URI(url).query.present?
+      url + "&utm_source=bsdweekly"
+    else
+      url + "?utm_source=bsdweekly"
     end
   end
 end

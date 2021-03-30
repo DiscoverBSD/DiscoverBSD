2eda1cfae9f551d86b967b490861945b3e0319ad
Jan Hovancik <conta.srdr@gmail.com>
2021-03-30 11:06:07 +0200

    Show oldest/newest articles in newsletter

--- app/controllers/admin/newsletter_controller.rb
+++ app/controllers/admin/newsletter_controller.rb
@@ -7,6 +7,8 @@ module Admin
       service = NextPostsService.new(minimum_of_posts: 1, end_date: 1.day.ago, number_of_days: 6)
       result = service.perform
       posts_service = PostsByDayService.new(result)
+      @newest = result.order(:created_at).first.created_at
+      @oldest = result.order(:created_at).last.created_at
       @posts = result.group_by { |post| post.newsletter_part }
     end
   end

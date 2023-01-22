3b8de6622f35946162f5702ef85fbd969d1b9ae2
jan <jan@hovancik.net>
2023-01-22 08:25:39 -0500

    Fixup tests

--- test/services/next_posts_service_test.rb
+++ test/services/next_posts_service_test.rb
@@ -2,13 +2,13 @@ require 'test_helper'
 
 class NextPostsServiceTest < ActiveSupport::TestCase
   test "it works ok with `number_of_days` as bigger value" do
-    service = NextPostsService.new(minimum_of_posts: 1, end_date: Time.zone.now, number_of_days: 10)
-    assert_equal 3, service.perform.size
+    service = NextPostsService.new(minimum_of_posts: 1, end_date: Time.zone.now, number_of_days: 20)
+    assert_equal 4, service.perform.size
   end
 
   test "it works ok with `minimum_of_posts` as bigger value" do
     service = NextPostsService.new(minimum_of_posts: 2, end_date: Time.zone.now, number_of_days: 1)
-    assert_equal 1, service.perform.size
+    assert_equal 3, service.perform.size
   end
 
   test "posts are from the newest" do

dd0a0e9745c8cf82906d9dfd8cb4e94b2ff88631
Jan Hovancik <jan@hovancik.net>
2025-01-08 10:03:13 +0000

    Only use image

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,13 +1,4 @@
 module ApplicationHelper
-  def og_image_url(title)
-    "https://og.tailgraph.com/og?fontFamily=Roboto&title=#{u title} \
-    &titleTailwind=text-gray-800%20font-bold%20text-6xl \
-    &bgUrl=https%3A%2F%2Fdiscoverbsd.com%2Fimg%2Fnik-shuliahin-rkFIIE9PxH0-unsplash.jpg \
-    &bgTailwind=bg-white \
-    &overlay=1 \
-    &overlayTailwind=bg-yellow-200%20opacity-30"
-  end
-
   def discoverbsd_utm_source_url url
     if URI(url).query.present?
       url + "&utm_source=discoverbsd"

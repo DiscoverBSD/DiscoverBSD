03837381be13eec2a149635afbf887f90e7ace36
Jan Hovancik <jan@hovancik.net>
2025-12-11 08:33:24 +0000

    Up deps

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -7,8 +7,15 @@ Rails.application.config.assets.version = '1.0'
 # Rails.application.config.assets.paths << Emoji.images_path
 # Add Yarn node_modules folder to the asset load path.
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
+# Ensure Font Awesome webfonts are resolved to logical paths like /assets/fa-brands-400.woff2
+Rails.application.config.assets.paths << Rails.root.join('node_modules/@fortawesome/fontawesome-free/webfonts')
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
-# Rails.application.config.assets.precompile += %w( admin.js admin.css )
+Rails.application.config.assets.precompile += %w[
+  fa-solid-900.woff2
+  fa-regular-400.woff2
+  fa-brands-400.woff2
+  fa-v4compatibility.woff2
+]
